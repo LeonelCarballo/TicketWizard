1da1b93b85d282c854affe83d7542bd00beb5cf0
Andrea <andrea.melendrez252774@potros.itson.edu.mx>
2025-02-19 23:02:40 -0700

    jiji

---
+++ src/main/java/itson/ticketwizard/persistencia/UsuarioDAO.java
@@ -0,0 +1,18 @@
+
+package itson.ticketwizard.persistencia;
+
+/**
+ *
+ * @author payde
+ */
+public class UsuarioDAO {
+    
+    private ManejadorConexiones manejadorConexiones;
+
+    public UsuarioDAO(ManejadorConexiones manejadorConexiones) {
+        this.manejadorConexiones = manejadorConexiones;
+    }
+       
+    public Usuario registrar(nuevoUsuarioDTO)
+    
+}
