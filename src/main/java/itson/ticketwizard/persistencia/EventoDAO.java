3581fe3ec06bcc6e47bb8db14f0e87683a58342b
dreamywiwo <danacg11@hotmail.com>
2025-02-20 00:17:19 -0700

    eventoDao

--- src/main/java/itson/ticketwizard/persistencia/EventoDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/EventoDAO.java
@@ -4,10 +4,69 @@
  */
 package itson.ticketwizard.persistencia;
 
+import itson.ticketwizard.dtos.EventoDTO;
+import itson.ticketwizard.entidades.Evento;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author Dana Chavez
  */
 public class EventoDAO {
+    private ManejadorConexiones manejadorConexiones;
     
+    public EventoDAO(ManejadorConexiones manejadorConexiones){
+        this.manejadorConexiones = manejadorConexiones;
+    }
+    
+    public List <Evento> obtenerEventos(){
+        String codigoSQL = """
+                            SELECT 
+                            	nombre, 
+                                nombreLocal, 
+                                descripcion, 
+                                fechaHora, 
+                                ciudad, 
+                                calle, 
+                                colonia, 
+                                codigoPostal 
+                            FROM Eventos;
+                           """;
+                            List <Evento> listaEventos = new LinkedList<>();
+                            
+                           try{
+                               Connection conexion = this.manejadorConexiones.crearConexion();
+                               PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+                               ResultSet resultadosConsulta = comando.executeQuery();
+                               
+                               // Nos movemos a cada una de las filas devueltas
+                               while(resultadosConsulta.next()){
+                                   // Estamos dentro de una fila
+                                   Integer codigo = resultadosConsulta.getInt("idEvento");
+                                   String nombre = resultadosConsulta.getString("nombre");
+                                   String nombreLocal = resultadosConsulta.getString("nombreLocal");
+                                   String descripcion = resultadosConsulta.getString("descripcion");
+                                   Date fechaHora = resultadosConsulta.getDate("fechaHora");
+                                   String ciudad = resultadosConsulta.getString("ciudad");
+                                   String calle = resultadosConsulta.getString("calle");
+                                   String colonia = resultadosConsulta.getString("colonia");
+                                   String codigoPostal = resultadosConsulta.getString("codigoPostal");
+                                   
+                                   
+                                   // Paquete donde empaquetaras los datos
+                                   Evento evento = new Evento(nombre, nombreLocal, descripcion, fechaHora, ciudad, calle, colonia, codigoPostal);
+                                   listaEventos.add(evento);
+                               }
+                               
+                           } catch (SQLException ex){
+                               System.err.println("Erros la consultar los artistas: " + ex.getMessage());
+                           }    
+                            return listaEventos;
+    }
 }
