115142b9ee7320e2be4dd7461d154372ce25de2b
LeonelCarballo <leonel.carballo235352@potros.itson.edu.mx>
2025-02-19 23:42:01 -0700

    BoletoDAO consultarBoletos

--- src/main/java/itson/ticketwizard/persistencia/BoletoDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/BoletoDAO.java
@@ -1,13 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package itson.ticketwizard.persistencia;
+
+import itson.ticketwizard.entidades.Boleto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
  * @author Dana Chavez
  */
 public class BoletoDAO {
+    
+    private ManejadorConexiones manejadorConexiones;
+    
+    public BoletoDAO(ManejadorConexiones manejadorConexiones){
+        this.manejadorConexiones = manejadorConexiones;
+    }
+    
+    public List <Boleto> consultarArtistas(){
+        String codigoSQL = """
+                            SELECT
+                                codigoArtista,
+                                nombre,
+                                nacionalidad,
+                                tipo,
+                                añoInicio
+                           FROM artistasNuevos;
+                           """;
+                            List <Boleto> listaBoletos = new LinkedList<>();
+                           try{
+                               Connection conexion = this.manejadorConexiones.crearConexion();
+                               PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+                               ResultSet resultadosConsulta = comando.executeQuery();
+                               // nos movemos a cada una de las filas devueltas
+                               while(resultadosConsulta.next()){
+                                   // estamos dentro de una fila
+                                   Integer numSerie = resultadosConsulta.getInt("numSerie");
+                                   String nombreDueño = resultadosConsulta.getString("idUsuario");
+                                   float precio = resultadosConsulta.getFloat("precio");
+                                   String disponibilidad = resultadosConsulta.getString("disponibilidad");
+                                   Integer numeroAsiento = resultadosConsulta.getInt("numeroAsiento");
+                                   String fila = resultadosConsulta.getString("fila");
+                                   Integer numControl = resultadosConsulta.getInt("numControl");
+                                   
+                                   // paquete donde empaquetaras los datos
+                                   Boleto boletos = new Boleto(numSerie,nombreDueño,precio,disponibilidad,numeroAsiento,fila,numControl);
+                                   listaBoletos.add(boletos);
+                               }
+                               
+                           } catch (SQLException ex){
+                               System.err.println("Erros la consultar los artistas: " + ex.getMessage());
+                           }    
+                            return listaBoletos;
+    }
     
 }
